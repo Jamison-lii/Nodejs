80fa8c9cce1391ab97942e518a578169a51c4fce
Jamison-Lii <ntuvlii5@gmail.com>
2025-02-08 07:15:39 +0100

    third commit about routing

--- Client and Server/server.js
+++ Client and Server/server.js
@@ -9,10 +9,23 @@ const server = http.createServer((req, res)=>{
   
 res.setHeader('Content-Type', 'text/html');
 
+let path = './views/';
+switch(req.url){
+    case '/':
+     path += 'index.html';
+     break;
+    case '/about':
+     path += 'about.html';
+     break;
+    default:
+        path += '404.html';
+        break;
+}
+
 /*res.write('<p>hello, ninjas<p/>');
 res.write('<p>hello, again ninjas<p/>');
 res.end();*/
-fs.readFile('./views/index.html', (err, data) => {
+fs.readFile(path, (err, data) => {
     if(err){
         console.log(err);
     }
